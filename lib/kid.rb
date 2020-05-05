9ba0f4de5f03c88f4147cf99740ded31e53c3bfd
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-05 20:46:28 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,9 +4,9 @@ require_relative './fancy_dance.rb'
 class Kid
 extend FancyDance::ClassMethods
 include FancyDance::InstanceMethods
-#attr_accessor :name
+attr_accessor :name
 
-#def initialize(name)
-#  @name = name
-#end
+def initialize(name)
+  @name = name
+end
 end
