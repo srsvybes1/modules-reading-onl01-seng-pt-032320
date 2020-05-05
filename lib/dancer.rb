8123a1e8f6f561179cbdf05f5af4aecdcf0de181
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-05 20:46:21 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,9 +5,9 @@ class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 
-#  attr_accessor :name
+  attr_accessor :name
 
-#  def initialize(name)
-#    @name = name
-  #end
+  def initialize(name)
+    @name = name
+  end
 end
